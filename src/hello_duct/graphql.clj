278de97f6fc7ecf50fec161069f2978a5b4f5c5e
Hikaru NASHIDA <nashida@xcoo.jp>
2019-05-10 13:28:31 +0900

    Add service map for graphql

---
+++ src/hello_duct/graphql.clj
@@ -0,0 +1,26 @@
+(ns hello-duct.graphql
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [com.walmartlabs.lacinia.pedestal :as pedestal]
+            [com.walmartlabs.lacinia.schema :as schema]
+            [com.walmartlabs.lacinia.util :as util]
+            [integrant.core :as ig]))
+
+;; TODO: add resolvers
+(def resolver-map
+  {:BoardGame_designers (constantly [])
+   :Designer_games (constantly [])
+   :query_game-by-id (constantly nil)})
+
+(defmethod ig/init-key ::schema
+  [_ _]
+  (-> "hello_duct/cgg-schema.edn"
+      io/resource
+      slurp
+      edn/read-string
+      (util/attach-resolvers resolver-map)
+      schema/compile))
+
+(defmethod ig/init-key ::service
+  [_ {:keys [schema options]}]
+  (pedestal/service-map schema options))
