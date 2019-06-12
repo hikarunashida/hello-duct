87f7cf31c709d4ce4787e91af16765316adceabe
Hikaru NASHIDA <nashida@xcoo.jp>
2019-06-12 18:59:47 +0900

    Add the resolver and query handler for graphql
    N.B. after the initialization, eval
    
    ((:hello-duct.graphql/execute system) {{query}})
    
    Squashed commit of the following:
    
    commit afafb1461bb8ba1ec1a1a22490aad638cc3ac05c
    Author: Hikaru NASHIDA <nashida@xcoo.jp>
    Date:   Wed Jun 12 18:58:29 2019 +0900
    
        Add the query executer
    
    commit c5c0f270eaac14b0398b7ff0219b809c3e670cdd
    Author: Hikaru NASHIDA <nashida@xcoo.jp>
    Date:   Wed Jun 12 18:09:45 2019 +0900
    
        Add the config for graphql
    
    commit e94c8d9e64650d9529110dcc668b7c5da76ab356
    Author: Hikaru NASHIDA <nashida@xcoo.jp>
    Date:   Wed Jun 12 18:06:31 2019 +0900
    
        Add the resolver-map and schema for graphql

--- src/hello_duct/graphql.clj
+++ src/hello_duct/graphql.clj
@@ -1,25 +1,70 @@
 (ns hello-duct.graphql
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
+            [com.walmartlabs.lacinia :as lacinia]
             [com.walmartlabs.lacinia.pedestal :as pedestal]
             [com.walmartlabs.lacinia.schema :as schema]
             [com.walmartlabs.lacinia.util :as util]
             [integrant.core :as ig]))
 
-;; TODO: add resolvers
-(def resolver-map
-  {:BoardGame_designers (constantly [])
-   :Designer_games (constantly [])
-   :query_game-by-id (constantly nil)})
+(defmethod ig/init-key ::load-data
+  [_ {:keys [data-path]}]
+  (prn [data-path])
+  (-> data-path
+      io/resource
+      slurp
+      edn/read-string))
 
-(defmethod ig/init-key ::schema
-  [_ {:keys [schema-path]}]
+(defn- resolve-game-by-id
+  [games-map context args value]
+  (let [{:keys [id]} args]
+    (get games-map id)))
+
+(defn- resolve-board-game-designers
+  [designers-map context args board-game]
+  (->> board-game
+       :designers
+       (map designers-map)))
+
+(defn- resolve-designer-games
+  [games-map context args designer]
+  (let [{:keys [id]} designer]
+    (->> games-map
+         vals
+         (filter #(-> % :designers (contains? id))))))
+
+(defn- entity-map
+  [data k]
+  (reduce #(assoc %1 (:id %2) %2)
+          {}
+          (get data k)))
+
+(defmethod ig/init-key ::resolver-map
+  [_ {:keys [cgg-data]}]
+  (let [entity-map* (partial entity-map cgg-data)
+        games-map (entity-map* :games)
+        designers-map (entity-map* :designers)]
+    {:query_game-by-id (partial resolve-game-by-id games-map)
+     :BoardGame_designers (partial resolve-board-game-designers designers-map)
+     :Designer_games (partial resolve-designer-games games-map)}))
+
+(defmethod ig/init-key ::load-schema
+  [_ {:keys [schema-path resolver-map]}]
   (-> schema-path
       io/resource
       slurp
       edn/read-string
       (util/attach-resolvers resolver-map)
       schema/compile))
+
+;; execute
+
+(defmethod ig/init-key ::execute
+  [_ {:keys [schema]}]
+  (fn [query]
+    (lacinia/execute schema query nil nil)))
+
+;; service
 
 (defmethod ig/init-key ::service
   [_ {:keys [schema options]}]
