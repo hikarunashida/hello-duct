1a796ea147cfc2a6d5e1211782b177d8441ec8e7
Hikaru NASHIDA <nashida@xcoo.jp>
2019-06-13 12:02:47 +0900

    Fix the binding forms

--- src/hello_duct/handler/example.clj
+++ src/hello_duct/handler/example.clj
@@ -3,10 +3,12 @@
             [ataraxy.response :as response] 
             [integrant.core :as ig]))
 
-(defmethod ig/init-key :hello-duct.handler/example [_ options]
+(defmethod ig/init-key :hello-duct.handler/example
+  [_ options]
   (fn [{[_] :ataraxy/result}]
     [::response/ok {:example "data"}]))
 
-(defmethod ig/init-key :hello-duct.handler/hello [_ options]
+(defmethod ig/init-key :hello-duct.handler/hello
+  [_ options]
   (fn [{[_] :ataraxy/result}]
     [::response/ok "<h1>Hello World</h1>"]))
