720a266b06a836bb28069ce32318f352bd659c17
Hikaru NASHIDA <nashida@xcoo.jp>
2019-06-25 19:26:32 +0900

    Add middlewares

--- src/hello_duct/ring.clj
+++ src/hello_duct/ring.clj
@@ -1,11 +1,18 @@
 (ns hello-duct.ring
   (:require [integrant.core :as ig]
             [reitit.ring :as r-ring]
+            [reitit.ring.coercion :as r-ring-corercion]
             [ring.adapter.jetty :as jetty]))
 
 (defmethod ig/init-key ::router
   [_ {:keys [routes opts]}]
   (r-ring/router routes opts))
+
+(defmethod ig/init-key ::middlewares
+  [_ _]
+  [r-ring-corercion/coerce-exceptions-middleware
+   r-ring-corercion/coerce-request-middleware
+   r-ring-corercion/coerce-response-middleware])
 
 (defn- error-response
   [status-code msg]
